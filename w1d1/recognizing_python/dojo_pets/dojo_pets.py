911281519b02bf05ffa6fa960b79dd350ed79e77
Taylor Bisett <taylorbisett@gmail.com>
2021-07-08 14:59:51 -0700

    iteration 1

---
+++ w1d1/recognizing_python/dojo_pets/dojo_pets.py
@@ -0,0 +1,48 @@
+class Pet:
+    def __init__(self, name, type, tricks, health, energy):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+
+    def sleep(self):
+        self.energy += 25
+
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        print(f" {self.health} {self.energy}")
+
+    def play(self):
+        self.health += 5
+        print(self.health)
+
+    def noise(self):
+        print("Woof Woof")
+
+
+class Ninja:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+
+    def walk(self):
+        self.pet.play()
+
+    def feed(self):
+        self.pet.eat()
+
+    def bathe(self):
+        self.pet.noise()
+
+
+taylor = Ninja("Taylor", "Bisett", Pet("Summer", "cocker spaniel", "barking", 100, 80), "Dog Biscuits", "Blue Buffalo")
+
+
+taylor.feed()
+taylor.bathe()
+taylor.walk()
