de3f6fe19070b1b41fdfd619acd0d710c4543f01
Taylor Bisett <taylorbisett@gmail.com>
2021-07-11 19:04:56 -0700

    iteration 2

--- Flask/fundamentals/checkerboard/server.py
+++ Flask/fundamentals/checkerboard/server.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
+
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def index(x = 8, y = 8, color1 = "red", color2 = "black"):
+    return render_template("index.html", x = 8, y = 8, color1 = "red", color2 = "black")
 
 @app.route('/<int:x>')
 def four(x = 4):
-    return render_template("index.html", x = x)
+    return render_template("index.html", x = x, y = 8, color1 = "red", color2 = "black")
 
 @app.route('/<int:x>/<int:y>')
 def anyNum(x = 4, y = 4):
